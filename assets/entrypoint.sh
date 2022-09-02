34508fa46f5d953e7997d34b1b96b1a040fa697f
cnocmpls <111349969+cnocmpls@users.noreply.github.com>
2022-09-02 20:29:55 +0530

    Update entrypoint.sh

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -46,7 +46,7 @@ fi
 #for service in ${SIAB_SERVICE}; do
 #	COMMAND+=" -s \"${service}\""
 #done
-COMMAND+=" -s \"${SIAB_SERVICE}\""
+COMMAND+=" -s ${SIAB_SERVICE}"
 if [ "$SIAB_SCRIPT" != "none" ]; then
 	set +e
 	/usr/bin/curl -s -k ${SIAB_SCRIPT} > /prep.sh
